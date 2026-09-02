ccf1d47d8203934fdec30cf7c6145d374be63053
adityaulemale <adityaulemale@gmail.com>
2026-09-03 01:25:34 +0530

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="pricing-intelligence",
+    version="0.0.1",
+    author="Aditya Santosh Ulemale",
+    author_email="",
+    description="Demand Prediction and Dynamic Pricing Intelligence System",
+    packages=find_packages(),
+    install_requires=[],
+)
